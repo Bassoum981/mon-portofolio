189d9321aa0e8a0c92b8cea14b4101d2b56e3b14
Ibrahim Bassoum <Ibrahimba23@icloud.com>
2026-06-16 00:15:37 +0000

    feat: mise en place de l'architecture multi-pages (PageController et routes)

---
+++ resources/views/projets.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+    <section class="max-w-6xl mx-auto px-6 py-20">
+        <h1 class="text-4xl font-bold mb-10">Mes Projets</h1>
+        <p class="text-gray-500">Ici apparaîtront tous mes projets de génie logiciel.</p>
+        
+        </section>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/', [PageController::class, 'home']);
+Route::get('/projets', [PageController::class, 'projets']);
