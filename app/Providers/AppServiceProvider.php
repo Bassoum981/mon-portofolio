9b50ed428c56687e8c971a5fe58e26079bf8d89c
Ibrahim Bassoum <Ibrahimba23@icloud.com>
2026-08-16 23:09:54 +0000

    Forcer HTTPS et corriger l'URL de base

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,9 +3,13 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
+
 {
+
+
     /**
      * Register any application services.
      */
@@ -17,8 +21,10 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
-    {
-        //
+  public function boot(): void
+{
+    if (env('APP_ENV') === 'production') {
+        URL::forceScheme('https');
     }
+}
 }
